62e61368cef78b5a604fbf73376192740b6131ff
wessy <wessya@gmail.com>
2024-07-31 01:12:40 +1000

    Добавлено ограничение по вводу импута мышки

--- Match_3/Game/InputHandler.cs
+++ Match_3/Game/InputHandler.cs
@@ -23,31 +23,41 @@ namespace Match_3
                     int x = ((e.X - 300) / drawing.CellSize);
                     int y = ((e.Y - 10) / drawing.CellSize);
 
-                    if (drawing.gameplay.SelectElem is null) drawing.gameplay.SelectElem = new Position(x, y);
-                    else
-                    {
-                        int x0 = drawing.gameplay.SelectElem.Value.x;
-                        int y0 = drawing.gameplay.SelectElem.Value.y;
-
-                        Position position = new Position(0,0);
-
-                        switch (GetDirection(x, x0, y, y0))
-                        {
-                            case Direction.Up: position = Position.Up; break;
-                            case Direction.Left: position = Position.Left; break;
-                            case Direction.Down: position = Position.Down; break;
-                            case Direction.Right: position = Position.Right; break;
-                            case Direction.Non: position = Position.Non; break;
-                        }
-                        
-                        if (position != Position.Non)
-                        {
-                            drawing.gameplay.TrySwichElem = new Position(x, y);
-                            drawing.gameplay.SwipeElementCoord(position,-position);                            
-                        }
+                    if (drawing.gameplay.ElemMatrix[x][y].OnPosition) {
+                        if (drawing.gameplay.SelectElem is null) drawing.gameplay.SelectElem = new Position(x, y);
                         else
                         {
-                            drawing.gameplay.SelectElem = null;
+                            int x0 = drawing.gameplay.SelectElem.Value.x;
+                            int y0 = drawing.gameplay.SelectElem.Value.y;
+
+                            if (drawing.gameplay.ElemMatrix[x0][y0].OnPosition)
+                            {
+                                Position position = new Position(0, 0);
+
+                                switch (GetDirection(x, x0, y, y0))
+                                {
+                                    case Direction.Up: position = Position.Up; break;
+                                    case Direction.Left: position = Position.Left; break;
+                                    case Direction.Down: position = Position.Down; break;
+                                    case Direction.Right: position = Position.Right; break;
+                                    case Direction.Non: position = Position.Non; break;
+                                }
+
+                                if (position != Position.Non)
+                                {
+                                    drawing.gameplay.TrySwichElem = new Position(x, y);
+                                    drawing.gameplay.SwipeElementCoord(position, -position);
+                                }
+                                else
+                                {
+                                    drawing.gameplay.SelectElem = null;
+                                }
+
+                            }
+                            else
+                            {
+                                drawing.gameplay.SelectElem = null;
+                            }
                         }
                     }
                 }
